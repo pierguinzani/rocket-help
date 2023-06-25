d39362aaaf97e0c891dda3b862d286da2f65b209
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-06-25 02:49:20 -0300

    feat: add SignIn Screen, Button and Input components

---
+++ src/screens/SignIn.jsx
@@ -0,0 +1,36 @@
+import { Text, View } from "native-base";
+import IonicIcons from 'react-native-vector-icons/Ionicons';
+import Logo from '../assets/Logo.svg';
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
+
+
+
+
+
+export function SignIn() {
+  return (
+    <View bgColor={"#202024"} flex={1} justifyContent={"center"} alignItems={"center"}>
+      <Logo /> 
+        <Text marginTop={"80px"} color={"#E1E1E6"} fontWeight={"bold"} fontSize={"16px"} >Acesse sua conta</Text>
+      <Input 
+        marginTop={"24px"}
+        fontSize={"16px"}
+        placeholder={"E-mail"}
+        InputLeftElement={<IonicIcons name="mail-outline" color={"#7C7C8A"} marginLeft={16} size={24} /> }
+      />
+      <Input 
+        InputLeftElement={<IonicIcons name="key-outline" color={"#7C7C8A"} marginLeft={16} size={20.25}/> }
+        placeholder={"Senha"}
+        fontSize={"16px"}
+        secureTextEntry
+        marginTop={"16px"} 
+        marginBotton={"32px"}
+        />
+      <Button 
+        marginBottom={"260px"}
+      />
+
+    </View>
+  )
+}
