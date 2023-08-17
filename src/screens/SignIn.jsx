d02d961c3134ba210f3746cf16d0684d2cf4edce
VitorNunesCPT <vitornunescpt@gmail.com>
2023-08-17 15:26:01 -0300

    :art: Correct format

--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -1,5 +1,5 @@
 
-import { VStack, Heading,Box,Image } from 'native-base';
+import { VStack, Heading, Box, Image } from 'native-base';
 
 
 
@@ -10,46 +10,46 @@ import { Button } from '../components/Button';
 import IonicIcons from 'react-native-vector-icons/Ionicons';
 
 
-export function SignIn({navigation}) {
-    function handleSignIn(){
+export function SignIn({ navigation }) {
+    function handleSignIn() {
         navigation.navigate('Home');
     }
     return (
-        <VStack  flex={1} alignItems={"center"} justifyContent={"center"}  backgroundColor={"#202024"}>
+        <VStack flex={1} alignItems={"center"} justifyContent={"center"} backgroundColor={"#202024"}>
 
             <Box
-            marginBottom={30}
+                marginBottom={30}
             >
-            <Image
-                source={img}
-                width={163}
-                height={87}
-                alt="Alternate Text" />
+                <Image
+                    source={img}
+                    width={163}
+                    height={87}
+                    alt="Alternate Text" />
             </Box>
 
             <Heading marginBottom={10} color={"#E1E1E6"} >
                 Acesse sua conta
             </Heading>
             <Input
-            placeholder="E-mail"
-            InputLeftElement={<IonicIcons name="mail" size={20} color="#7C7C8A" marginLeft={20}/>}
-            keyboardType='email-address'
-            autoCapitalize='none'
+                placeholder="E-mail"
+                InputLeftElement={<IonicIcons name="mail" size={20} color="#7C7C8A" marginLeft={20} />}
+                keyboardType='email-address'
+                autoCapitalize='none'
             />
 
             <Input
-            placeholder="Senha"
-            InputLeftElement={<IonicIcons name="key" size={20} color="#7C7C8A" marginLeft={20}/>}
-            secureTextEntry
+                placeholder="Senha"
+                InputLeftElement={<IonicIcons name="key" size={20} color="#7C7C8A" marginLeft={20} />}
+                secureTextEntry
 
             />
 
             <Button
-            width={"90%"}
-            onPress={handleSignIn}
-            title={"Entrar"}
+                width={"90%"}
+                onPress={handleSignIn}
+                title={"Entrar"}
             />
 
-        </VStack>    
+        </VStack>
     )
 }
