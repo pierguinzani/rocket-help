e8214d4d47faeea9113b2d8be9c7f76790275358
VitorNunesCPT <vitornunescpt@gmail.com>
2023-08-23 20:43:54 -0300

    :sparkles: Notification in NewOrder

--- src/screens/NewOrder.jsx
+++ src/screens/NewOrder.jsx
@@ -2,9 +2,11 @@ import { VStack, HStack, Text, IconButton, Box } from 'native-base';
 import IonicIcons from 'react-native-vector-icons/Ionicons';
 import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import notifee, { AndroidImportance} from '@notifee/react-native';
 
 import { Button } from '../components/Button';
 import { Input } from '../components/Input';
+
 
 export function NewOrder() {
     const navigation = useNavigation();
@@ -12,15 +14,33 @@ export function NewOrder() {
         navigation.navigate('Home');
     }
 
-    function handleCadastrar() {
-        navigation.navigate('Home', {
-          patrimonio: patrimonio,
-          descricao: descricao,
-        });
-      }
+      
+  const [patrimonio, setPatrimonio] = useState('');
+  const [descricao, setDescricao] = useState('');
 
-    const [patrimonio, setPatrimonio] = useState('');
-    const [descricao, setDescricao] = useState('');
+  const handleCadastrar = async () => {
+    const channelId = await notifee.createChannel({
+      id: 'login',
+      name: 'Default Channel',
+      importance: AndroidImportance.HIGH,
+    });
+    await notifee.displayNotification({
+      title: 'Ticket criado!',
+      body: 'Um novo ticket foi registrado em nosso sistema.',
+      android: {
+        channelId,
+        smallIcon: 'ic_stat_onesignal_default',
+        color : '#00B37E',
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+    navigation.navigate('Home', {
+      patrimonio: patrimonio,
+      descricao: descricao,
+    });
+  }  
 
     return (
     <VStack
